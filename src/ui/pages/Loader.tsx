5cb4418a1f6cb00a690c78acafb2064d5e489958
Sara Morillon <sara.morillon.pro@gmail.com>
2023-02-26 10:19:46 +0100

    Increase main nav bottom border width

--- src/ui/pages/Loader.tsx
+++ src/ui/pages/Loader.tsx
@@ -3,35 +3,34 @@ export function Loader() {
     <section>
       <h2>Simple loader</h2>
       <div>
-        <span aria-busy />
+        <span aria-busy aria-label="Loading..." />
       </div>
       <h2>Loading button</h2>
       <button aria-busy>Send</button>
-      <strong>
-        <button aria-busy>Send</button>
-      </strong>
+      <button aria-busy data-variant="primary">
+        Send
+      </button>
       <h2>Loading article</h2>
       <article aria-busy />
       <h2>Loading container</h2>
       <div aria-busy />
       <h2>Loading input</h2>
       <label aria-busy>
-        Coucou <input disabled />
+        <input disabled />
       </label>
       <h2>Loading select</h2>
       <label aria-busy>
-        Coucou
         <select disabled>
           <option>Option</option>
         </select>
       </label>
       <h2>Loading checkbox</h2>
       <label>
-        <input type="checkbox" aria-busy disabled /> Coucou
+        <input type="checkbox" aria-busy disabled /> Checkbox
       </label>
       <h2>Loading radio button</h2>
       <label>
-        <input type="radio" aria-busy disabled /> Coucou
+        <input type="radio" aria-busy disabled /> Radio button
       </label>
     </section>
   )
